80409767418bde6a85bc8e837f977740f3e42e6d
VishaLNotOp <samantavishal0508@gmail.com>
2024-02-01 18:08:13 +0530

    Create spectrum.c

---
+++ spectrum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+ {
+    int a;
+    
+    printf(" enter the number : 1 \n");
+    printf(" enter the number : 2 \n");
+    printf(" enter the number :  \n" );
+    scanf ("%d",&a);
+    
+    printf("you press %d \n",a);
+   
+   if (a == 1)
+     {
+        printf("take magepunk skin");
+   } 
+
+      else if (a== 2)
+
+    {
+        printf("take spectrum skin");
+   }
+return 0;
+}
